29af0eb5d015c7dc07dd5ca54ce3729d5b189fe7
lucazpinheiro <lucascpinheiro10@gmail.com>
2020-10-24 22:25:32 -0300

    Refactor plus-minus solution

--- plus-minus/solution.js
+++ plus-minus/solution.js
@@ -2,24 +2,27 @@
  * problem: https://www.hackerrank.com/challenges/plus-minus/problem
  */
 
-const sampleInput = [-4, 3, -9, 0, 4, 1];
+// const sampleInput = [-4, 3, -9, 0, 4, 1];
+
+const sampleInput = [1, 1, 0, -1, -1];
+
 
 function plusMinus(arr) {
-  const positiveNumbers = arr.reduce((acc, element) => {
-    if (element > 0) acc += 1;
+  const numbers = arr.reduce((acc, element) => {
+    if (element > 0) acc.positive += 1;
+    if (element < 0) acc.negative += 1;
+    if (element === 0) acc.zeros += 1;
+
     return acc;
-  }, 0);
-  const negativeNumbers = arr.reduce((acc, element) => {
-    if (element < 0) acc += 1;
-    return acc;
-  }, 0);
-  const zeros = arr.reduce((acc, element) => {
-    if (element === 0) acc += 1;
-    return acc;
-  }, 0);
-  console.log((positiveNumbers / arr.length).toFixed(6));
-  console.log((negativeNumbers / arr.length).toFixed(6));
-  console.log((zeros / arr.length).toFixed(6));
+  }, {
+    positive: 0,
+    negative: 0,
+    zeros: 0,
+  });
+
+  console.log((numbers.positive / arr.length).toFixed(6));
+  console.log((numbers.negative / arr.length).toFixed(6));
+  console.log((numbers.zeros / arr.length).toFixed(6));
 }
 
 plusMinus(sampleInput);
